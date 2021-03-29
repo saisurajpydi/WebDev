e55dcc0de024caf5fa950f57d9c36695ed3c35d8
saisurajpydi <saisurajpydi@gmail.com>
2021-03-29 12:11:46 +0530

    web Dev Bootcamp

---
+++ TrailWeb JS/demo.js
@@ -0,0 +1,33 @@
+document.write("<h1>hello world</h1>");
+document.write(2+3);
+//this is a comment
+/*
+below are variables used in js
+*/
+var name = "suraj";
+var age = 28;
+var bool = true;
+document.write(name,age,bool);
+//alert(age)
+document.write("<br>");
+// this is operators
+var a = 1 ;
+var b = 2 ;
+var c = "sai";
+var d = 10;
+var result = a + b + d;
+var re1 = a + b + c ;
+var re2 = c+a+b; 
+document.write(result);
+document.write("<br>");
+document.write(re1);
+document.write("<br>");
+document.write(re2);
+document.write("<br>");
+document.write(a+b*d);
+document.write("<br>");
+document.write((a+b)*d);
+
+// control statements
+document.write("<br>");
+document.write("<br>");)
